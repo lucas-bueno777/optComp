d4f508e632a789abdc8ab3f29312ac41cde951de
Lucas B <lucasbuenoteixeira@gmail.com>
2023-05-20 16:32:20 -0300

    First upload containing base development

---
+++ file_manager.py
@@ -0,0 +1,111 @@
+"""
+v.1.0.0 - Basic release
+*.inp file manager for *ORIENTATION, *NSET and *ELSET parameters extraction and riting
+"""
+
+import numpy as np
+
+class FileProcessor:
+    """Processes the input file reading and writing data"""
+
+    def __init__(self) -> None:
+        """Initialization of local class variables"""
+        self.read_lines = None
+
+    def read_file(self, file_path: str) -> None:
+        """Opens the specified file and stores its data in a string"""
+        try:
+            with open(file_path, 'r') as file:
+                self.read_lines = file.readlines()
+
+        except FileNotFoundError:
+            print("Sorry, the file path cannot be found or is invalid.")
+
+    def search_sets(self, set_type: str) -> list:
+        """Searches for sets inside the file and stores its name and number of elements"""
+        sets = []
+        data_values = []
+
+        for i in range(len(self.read_lines)):
+            line = self.read_lines[i]
+
+            # If a set is found
+            if set_type in line.upper():
+                split_line = line.split("=")
+                nset_name = split_line[1].strip()
+
+                # Counts how many lines after the *NSET starts with a number to count them
+                lines_of_data = 0
+                for j in range(i+1, len(self.read_lines)):
+                    first_character = self.read_lines[j].strip()[0]
+                    if first_character.isdigit():
+                        lines_of_data += 1
+                    else:
+                        break
+
+                # Counts how many nodes/elements are inside the set
+                count_numbers = 0
+                for j in range(i+1, i+1+lines_of_data):
+                    line_splitted_by_commas = self.read_lines[j].strip().split(",")
+                    # Removes residual spaces from operation
+                    if "" in line_splitted_by_commas:
+                        line_splitted_by_commas.remove("")
+                    count_numbers += len(line_splitted_by_commas)
+
+                # Stores the sets name and contents inside the lists
+                sets.append(nset_name)
+                data_values.append(count_numbers)
+
+        return sets, data_values
+
+    def search_orientation(self) -> list:
+        """Searches for orientation cards inside the input file"""
+        self.orientation_line = []
+        orientation_list =[]
+        x_local_list = []
+        y_local_list = []
+        z_local_list = []
+        x_angle_list = []
+        y_angle_list = []
+        z_angle_list = []
+
+        for i in range(len(self.read_lines)):
+            line = self.read_lines[i]
+
+            # If a orientation card is found
+            if "*ORIENTATION" in line.upper():
+                split_line = line.split("=")
+                orientation_name = split_line[-1].strip()
+                input_line = self.read_lines[i+1].split(",")
+                input_line = [float(num) for num in input_line]
+
+                # Vector manipulations
+                vector_x = np.array(input_line[:3])
+                point_xy = np.array(input_line[-3:])
+                x_local = vector_x / np.linalg.norm(vector_x)
+                z_local = np.cross(x_local, point_xy) / np.linalg.norm(np.cross(x_local, point_xy))
+                y_local = np.cross(z_local, x_local)
+
+                # Angle calculation
+                angle_x = np.degrees(np.arccos(np.dot(x_local, np.array([1, 0, 0]))))
+                angle_y = np.degrees(np.arccos(np.dot(y_local, np.array([0, 1, 0]))))
+                angle_z = np.degrees(np.arccos(np.dot(z_local, np.array([0, 0, 1]))))
+
+                # List append
+                orientation_list.append(orientation_name)
+                x_local_list.append(x_local.tolist())
+                y_local_list.append(y_local.tolist())
+                z_local_list.append(z_local.tolist())
+                x_angle_list.append(angle_x)
+                y_angle_list.append(angle_y)
+                z_angle_list.append(angle_z)
+                self.orientation_line.append(i)
+
+        return orientation_list, x_local_list, y_local_list, z_local_list, x_angle_list, y_angle_list, z_angle_list
+
+    def write_new_orientation(self, input_inp) -> ".inp":
+        a=1
+
+debug = FileProcessor()
+debug.read_file("Shell_3_sections_flipped.inp")
+debug.search_orientation()
---
+++ general_interface.py
@@ -0,0 +1,211 @@
+"""
+v.1.0.0 - Basic release
+GUI interface for the composite optimizer. Contains input, optimization and status tabs.
+"""
+
+import sys
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtWidgets import (
+    QFileDialog,
+    QWidget,
+    QPushButton,
+    QGridLayout,
+    QVBoxLayout,
+    QHBoxLayout,
+    QLabel,
+    QApplication,
+    QDialog,
+    QTabWidget,
+    QLineEdit,
+    QFrame,
+    QListWidget,
+    QGroupBox,
+)
+from file_manager import FileProcessor
+
+class SharedData:
+    """Comprises the shared data between tabs"""
+    def __init__(self) -> None:
+        self.path_inp = None
+
+
+class MainWindow(QDialog):
+    """
+    Main window definition. Contains the three tabs and window layout
+    and title definitions 
+    """
+    def __init__(self, parent: QWidget = None) -> None:
+        super().__init__(parent)
+
+        self.shared_data = SharedData()
+        self.input_tab = InputsTab(self.shared_data)
+        self.optimization_tab = OptimizationTab(self.shared_data)
+        self.status_tab = StatusTab(self.shared_data)
+
+        self.tab_widget = QTabWidget()
+        self.tab_widget.addTab(self.input_tab, "Inputs")
+        self.tab_widget.addTab(self.optimization_tab, "Optimization")
+        self.tab_widget.addTab(self.status_tab, "Status")
+
+        main_window_layout = QVBoxLayout()
+        main_window_layout.addWidget(self.tab_widget)
+        self.setLayout(main_window_layout)
+        self.setWindowTitle("optComp v.1.0.0")
+
+        # Size definitions
+        min_size = QSize(500, 500)
+        self.setMinimumSize(min_size)
+    
+    def update_optimization_tab(self):
+        self.optimization_tab.update_contents_opt()
+
+class InputsTab(QWidget):
+    """
+    Inputs tab definition. Comprises methods for open file explorer, 
+    read this file and call backend functions and use this results to
+    update values inside this tab
+    """
+    def __init__(self, shared_data_input) -> None:
+        super().__init__()
+
+        self.shared_data_input = shared_data_input
+
+        # First groupbox definition: comprises the input file manager
+        file_groupbox = QGroupBox("File input")
+
+        self.select_file_button = QPushButton("Select file")
+        self.select_file_button.clicked.connect(self.select_file)
+        self.file_path_line = QLineEdit("Select the input file...")
+        self.read_file_button = QPushButton("Read file")
+        self.read_file_button.clicked.connect(self.read_file_exec)
+
+
+        file_input_layout = QHBoxLayout()
+        file_input_layout.addWidget(self.select_file_button)
+        file_input_layout.addWidget(self.file_path_line)
+        file_input_layout.addWidget(self.read_file_button)
+        file_groupbox.setLayout(file_input_layout)
+
+        # Second groupbox definition: Regard the orientation cards
+        orientation_groupbox = QGroupBox("Orientation")
+        self.orientation_list = QListWidget()
+        self.orientation_list.itemClicked.connect(self.update_angles)
+
+        self.theta_x = QLabel("Angle \u03B8x")
+        self.theta_x_value = QLabel()
+        self.theta_x_value.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+        self.theta_y = QLabel("Angle \u03B8y")
+        self.theta_y_value = QLabel()
+        self.theta_y_value.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+        self.theta_z = QLabel("Angle \u03B8z")
+        self.theta_z_value = QLabel()
+        self.theta_z_value.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+        self.explanation = QLabel("Angles measured between local and global axis")
+
+        orientation_layout = QGridLayout()
+        orientation_layout.addWidget(self.orientation_list, 0, 0, 6, 1)
+        orientation_layout.setColumnStretch(0, 4)
+        orientation_layout.setColumnStretch(1, 1)
+        orientation_layout.setColumnStretch(2, 1)
+        orientation_layout.addWidget(self.theta_x, 0, 1)
+        orientation_layout.addWidget(self.theta_x_value, 0, 2)
+        orientation_layout.addWidget(self.theta_y, 1, 1)
+        orientation_layout.addWidget(self.theta_y_value, 1, 2)
+        orientation_layout.addWidget(self.theta_z, 2, 1)
+        orientation_layout.addWidget(self.theta_z_value, 2, 2)
+        orientation_layout.addWidget(self.explanation, 3, 1, 1, 2)
+        orientation_groupbox.setLayout(orientation_layout)
+
+        # Third groupbox definition: Element sets
+        elements_groupbox = QGroupBox("Element Sets")
+        self.elements_list = QListWidget()
+        elements_layout = QVBoxLayout()
+        elements_layout.addWidget(self.elements_list)
+        elements_groupbox.setLayout(elements_layout)
+
+        # Fourth groupbox definition: Node sets
+        nodes_groupbox = QGroupBox("Node sets")
+        self.nodes_list = QListWidget()
+        nodes_layout = QVBoxLayout()
+        nodes_layout.addWidget(self.nodes_list)
+        nodes_groupbox.setLayout(nodes_layout)
+
+        # Layout definition for the parent tab
+        inputs_tab_layout = QGridLayout()
+        inputs_tab_layout.addWidget(file_groupbox, 0, 0, 1, 2)
+        inputs_tab_layout.addWidget(orientation_groupbox, 1, 0, 1, 2)
+        inputs_tab_layout.addWidget(elements_groupbox, 2, 0)
+        inputs_tab_layout.addWidget(nodes_groupbox, 2, 1)
+        self.setLayout(inputs_tab_layout)
+
+    def select_file(self):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, 'Select File')
+        if file_path:
+            self.file_path_line.setText(file_path)
+
+    def read_file_exec(self):
+        self.shared_data_input.path_inp = self.file_path_line.text()
+        instance = FileProcessor()
+        instance.read_file(self.shared_data_input.path_inp)
+        self.orientation_list_data, self.x_local_list, self.y_local_list, self.z_local_list, \
+            self.x_angles, self.y_angles, self.z_angles = instance.search_orientation()
+        self.element_names, self.elements_numbers = instance.search_sets("*ELSET")
+        self.nodes_names, self.nodes_numbers = instance.search_sets("*NSET")
+
+        self.update_elements()
+        self.update_nodes()
+        self.update_orientation_list()
+        self.window().update_optimization_tab()
+
+    def update_orientation_list(self):
+        self.orientation_list.clear()
+        self.orientation_list.addItems(self.orientation_list_data)
+
+    def update_angles(self):
+        self.theta_x_value.clear()
+        index = self.orientation_list.currentRow()
+        self.theta_x_value.setText(str(format(self.x_angles[index], ".1f")))
+        self.theta_y_value.setText(str(format(self.y_angles[index], ".1f")))
+        self.theta_z_value.setText(str(format(self.z_angles[index], ".1f")))
+
+    def update_elements(self):
+        self.elements_list.clear()
+        list = [str(a) + " - " + str(b) + " elements" for a, b in zip(self.element_names, self.elements_numbers)]
+        self.elements_list.addItems(list)
+
+    def update_nodes(self):
+        self.nodes_list.clear()
+        list = [str(a) + " - " + str(b) + " nodes" for a, b in zip(self.nodes_names, self.nodes_numbers)]
+        self.nodes_list.addItems(list)
+
+class OptimizationTab(QWidget):
+    """
+    Docstring to be defined
+    """
+    def __init__(self, shared_data_opt) -> None:
+        super().__init__()
+
+        self.shared_data_opt = shared_data_opt
+        self.file_path_test = QLabel("oi")
+        optimization_layout = QHBoxLayout()
+        optimization_layout.addWidget(self.file_path_test)
+        self.setLayout(optimization_layout)
+    
+    def update_contents_opt(self):
+        self.file_path_test.setText(self.shared_data_opt.path_inp)
+
+class StatusTab(QWidget):
+    """
+    Docstring to be defined
+    """
+    def __init__(self, shared_data_status) -> None:
+        super().__init__()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    main_window = MainWindow()
+    main_window.show()
+
+    sys.exit(app.exec())
